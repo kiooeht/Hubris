1bad61695c1fc8e0c0415f4ef68de06f46c95381
Kiooeht <kiooeht@gmail.com>
2018-07-01 16:34:42 -0700

    Complete Hubris card

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/curses/Hubris.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/curses/Hubris.java
@@ -2,12 +2,17 @@ package com.evacipated.cardcrawl.mod.hubris.cards.curses;
 
 import basemod.abstracts.CustomCard;
 import com.evacipated.cardcrawl.mod.hubris.actions.common.AutoplayCardAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.vfx.combat.InflameEffect;
 
 public class Hubris extends CustomCard
 {
@@ -18,7 +23,6 @@ public class Hubris extends CustomCard
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
 
-    // TODO: Make Inescapable
     public Hubris()
     {
         super(ID, NAME, IMG, COST, DESCRIPTION, CardType.POWER, CardColor.CURSE, CardRarity.SPECIAL, CardTarget.SELF);
@@ -29,7 +33,9 @@ public class Hubris extends CustomCard
     @Override
     public void use(AbstractPlayer p, AbstractMonster m)
     {
-        // TODO: Do something
+        AbstractDungeon.actionManager.addToBottom(new VFXAction(p, new InflameEffect(p), 1.0F));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new StrengthPower(p, 2), 2));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new DexterityPower(p, -1), -1));
     }
 
     @Override
