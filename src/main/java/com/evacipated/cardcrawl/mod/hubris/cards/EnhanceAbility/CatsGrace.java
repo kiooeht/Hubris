1246d1662fae6f7ed8c033686f66e8e5fa1090cc
Kiooeht <kiooeht@gmail.com>
2018-07-02 16:27:25 -0700

    Card: Enhance Ability

---
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/EnhanceAbility/CatsGrace.java
@@ -0,0 +1,54 @@
+package com.evacipated.cardcrawl.mod.hubris.cards.EnhanceAbility;
+
+import com.evacipated.cardcrawl.mod.hubris.CardIgnore;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+
+@CardIgnore
+public class CatsGrace extends AbstractCard
+{
+    public static final String ID = "hubris:CatsGrace";
+    public static final String IMG = "green/power/footwork";
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    private int amt;
+    private int upgrade_amt;
+
+    public CatsGrace(int amount, int upgrade_amount)
+    {
+        super(ID, NAME, IMG, IMG, -2, DESCRIPTION, CardType.POWER, CardColor.RED, CardRarity.RARE, CardTarget.SELF);
+
+        magicNumber = baseMagicNumber = amount;
+        amt = amount;
+        upgrade_amt = upgrade_amount;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new DexterityPower(p, magicNumber), magicNumber));
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(upgrade_amt);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new CatsGrace(amt, upgrade_amt);
+    }
+}
---
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/EnhanceAbility/FoxsCunning.java
@@ -0,0 +1,54 @@
+package com.evacipated.cardcrawl.mod.hubris.cards.EnhanceAbility;
+
+import com.evacipated.cardcrawl.mod.hubris.CardIgnore;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.FocusPower;
+
+@CardIgnore
+public class FoxsCunning extends AbstractCard
+{
+    public static final String ID = "hubris:FoxsCunning";
+    public static final String IMG = "blue/power/defragment";
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    private int amt;
+    private int upgrade_amt;
+
+    public FoxsCunning(int amount, int upgrade_amount)
+    {
+        super(ID, NAME, IMG, IMG, -2, DESCRIPTION, CardType.POWER, CardColor.RED, CardRarity.RARE, CardTarget.SELF);
+
+        magicNumber = baseMagicNumber = amount;
+        amt = amount;
+        upgrade_amt = upgrade_amount;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new FocusPower(p, magicNumber), magicNumber));
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(upgrade_amt);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new FoxsCunning(amt, upgrade_amt);
+    }
+}
