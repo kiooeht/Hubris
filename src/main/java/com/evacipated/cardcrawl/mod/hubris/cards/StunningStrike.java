03677cec392e31de928cc432bdde924a5d8cab8a
Kiooeht <kiooeht@gmail.com>
2018-07-03 15:17:58 -0700

    Remove unnecessary import

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/StunningStrike.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/StunningStrike.java
@@ -5,7 +5,6 @@ import com.evacipated.cardcrawl.mod.hubris.powers.StunPower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.unique.CalculatedGambleAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
