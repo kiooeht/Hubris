35b47aa95be24fab919e8703218d1ecd900dcb3f
Kiooeht <kiooeht@gmail.com>
2018-07-08 15:19:06 -0700

    Show adding Hubris to starting deck

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/patches/HubrisAddToStartingDeck.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/patches/HubrisAddToStartingDeck.java
@@ -3,9 +3,10 @@ package com.evacipated.cardcrawl.mod.hubris.patches;
 import com.evacipated.cardcrawl.mod.hubris.cards.curses.Hubris;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.dungeons.Exordium;
-import com.megacrit.cardcrawl.unlock.UnlockTracker;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
 
 @SpirePatch(
         cls="com.megacrit.cardcrawl.dungeons.AbstractDungeon",
@@ -16,8 +17,7 @@ public class HubrisAddToStartingDeck
     public static void Postfix()
     {
         if (AbstractDungeon.floorNum <= 1 && CardCrawlGame.dungeon instanceof Exordium) {
-            AbstractDungeon.player.masterDeck.addToTop(new Hubris());
-            UnlockTracker.markCardAsSeen(Hubris.ID);
+            AbstractDungeon.topLevelEffects.add(new ShowCardAndObtainEffect(new Hubris(), Settings.WIDTH / 2.0f, Settings.HEIGHT / 2.0f));
         }
     }
 }
