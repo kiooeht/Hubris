1a839b1c61a4ed6a887181520b70d6ee3b0ad52e
Kiooeht <kiooeht@gmail.com>
2018-07-16 21:40:09 -0700

    Explain Herb in Bundle of Herbs tooltips

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/colorless/Herb.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/colorless/Herb.java
@@ -18,7 +18,7 @@ public class Herb extends CustomCard
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    private static final int COST = 1;
+    public static final int COST = 1;
     private static final int REGEN = 3;
     private static final int UPGRADE_REGEN = 3;
 
--- src/main/java/com/evacipated/cardcrawl/mod/hubris/relics/BundleOfHerbs.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/relics/BundleOfHerbs.java
@@ -3,6 +3,8 @@ package com.evacipated.cardcrawl.mod.hubris.relics;
 import com.evacipated.cardcrawl.mod.hubris.cards.colorless.Herb;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.GameDictionary;
+import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.megacrit.cardcrawl.rooms.RestRoom;
@@ -15,6 +17,12 @@ public class BundleOfHerbs extends AbstractRelic
     public BundleOfHerbs()
     {
         super(ID, "test2.png", RelicTier.RARE, LandingSound.FLAT);
+
+        tips.clear();
+        tips.add(new PowerTip(name, description));
+        tips.add(new PowerTip(Herb.NAME, Herb.NAME + " is a #b" + Herb.COST + " cost Skill card that gives #yRegen and is #yFleeting."));
+        tips.add(new PowerTip("Fleeting", GameDictionary.keywords.get("fleeting")));
+        initializeTips();
     }
 
     @Override
