4ccbba6db7aac659ab106688f1db6dc151fd3356
Kiooeht <kiooeht@gmail.com>
2018-07-17 15:18:58 -0700

    Card: Counter

---
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/actions/unique/CounterAction.java
@@ -0,0 +1,46 @@
+package com.evacipated.cardcrawl.mod.hubris.actions.unique;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.unique.SpotWeaknessAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.monsters.EnemyMoveInfo;
+import com.megacrit.cardcrawl.vfx.ThoughtBubble;
+
+import java.lang.reflect.Field;
+
+public class CounterAction extends AbstractGameAction
+{
+    private AbstractMonster targetMonster;
+
+    public CounterAction(AbstractMonster m)
+    {
+        duration = 0.0f;
+        actionType = ActionType.WAIT;
+        targetMonster = m;
+    }
+
+    @Override
+    public void update()
+    {
+        if (targetMonster != null
+                && (targetMonster.intent == AbstractMonster.Intent.ATTACK
+                || targetMonster.intent == AbstractMonster.Intent.ATTACK_BUFF
+                || targetMonster.intent == AbstractMonster.Intent.ATTACK_DEBUFF
+                || targetMonster.intent == AbstractMonster.Intent.ATTACK_DEFEND)) {
+            try {
+                Field f = AbstractMonster.class.getDeclaredField("move");
+                f.setAccessible(true);
+                EnemyMoveInfo move = (EnemyMoveInfo)f.get(targetMonster);
+                AbstractDungeon.actionManager.addToTop(new DamageAction(targetMonster, new DamageInfo(AbstractDungeon.player, move.baseDamage, damageType), AttackEffect.FIRE));
+            } catch (IllegalAccessException | NoSuchFieldException e) {
+                e.printStackTrace();
+            }
+        } else {
+            AbstractDungeon.effectList.add(new ThoughtBubble(AbstractDungeon.player.dialogX, AbstractDungeon.player.dialogY, 3.0f, SpotWeaknessAction.TEXT[0], true));
+        }
+        isDone = true;
+    }
+}
---
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/green/Counter.java
@@ -0,0 +1,46 @@
+package com.evacipated.cardcrawl.mod.hubris.cards.green;
+
+import basemod.abstracts.CustomCard;
+import com.evacipated.cardcrawl.mod.hubris.actions.unique.CounterAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class Counter extends CustomCard
+{
+    public static final String ID = "hubris:Counter";
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    private static final int COST = 2;
+    private static final int UPGRADE_COST = 1;
+
+    public Counter()
+    {
+        super(ID, NAME, null,  COST, DESCRIPTION, AbstractCard.CardType.ATTACK, CardColor.GREEN, AbstractCard.CardRarity.UNCOMMON, AbstractCard.CardTarget.ENEMY);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        AbstractDungeon.actionManager.addToBottom(new CounterAction(m));
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADE_COST);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new Counter();
+    }
+}
