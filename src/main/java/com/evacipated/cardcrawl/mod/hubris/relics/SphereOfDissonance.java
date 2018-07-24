d938f9eed1f87b52e3b4ec7299fa25e011932533
Kiooeht <kiooeht@gmail.com>
2018-07-23 18:04:09 -0700

    Rework Sphere of Dissonance

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/relics/SphereOfDissonance.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/relics/SphereOfDissonance.java
@@ -1,38 +1,35 @@
 package com.evacipated.cardcrawl.mod.hubris.relics;
 
-import com.evacipated.cardcrawl.mod.hubris.cards.colorless.MarkOfDissonance;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
 public class SphereOfDissonance extends AbstractRelic
 {
     public static final String ID = "hubris:SphereOfDissonance";
+    private static final int AMT = 3;
 
     public SphereOfDissonance()
     {
         super(ID, "test6.png", RelicTier.UNCOMMON, LandingSound.MAGICAL);
+    }
 
-        tips.clear();
-        tips.add(new PowerTip(name, description));
-        tips.add(new PowerTip(MarkOfDissonance.NAME, "A #b" + MarkOfDissonance.COST + " cost Skill card that applies #yDissonance."));
-        initializeTips();
+    @Override
+    public String getUpdatedDescription()
+    {
+        return DESCRIPTIONS[0] + AMT + DESCRIPTIONS[1];
     }
 
     @Override
     public void atBattleStart()
     {
         flash();
-        AbstractDungeon.actionManager.addToBottom(new RelicAboveCreatureAction(AbstractDungeon.player, this));
-        AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(new MarkOfDissonance(), 1, true, false));
-    }
-
-    @Override
-    public String getUpdatedDescription()
-    {
-        return DESCRIPTIONS[0];
+        AbstractMonster m = AbstractDungeon.getCurrRoom().monsters.getRandomMonster(true);
+        AbstractDungeon.actionManager.addToBottom(new RelicAboveCreatureAction(m, this));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, AbstractDungeon.player, new VulnerablePower(m, AMT, false), AMT, true));
     }
 
     @Override
