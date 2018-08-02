e80d1d6a67ba7e765d0d9bce4c7dd1549de096f6
Kiooeht <kiooeht@gmail.com>
2018-08-01 17:40:43 -0700

    Synergy: Bottled Heart + Necronomicon

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/patches/BottledHeartPatch.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/patches/BottledHeartPatch.java
@@ -6,9 +6,26 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.relics.Necronomicon;
 
 public class BottledHeartPatch
 {
+    @SpirePatch(
+            cls="com.megacrit.cardcrawl.relics.Necronomicon",
+            method="onEquip"
+    )
+    public static class NecronomiconSynergy
+    {
+        public static void Prefix(Necronomicon __instance)
+        {
+            if (AbstractDungeon.player.hasRelic(BottledHeart.ID)) {
+                AbstractRelic relic = AbstractDungeon.player.getRelic(BottledHeart.ID);
+                relic.setCounter(relic.counter + 10);
+                relic.flash();
+            }
+        }
+    }
+
     @SpirePatch(
             cls="com.megacrit.cardcrawl.core.AbstractCreature",
             method="increaseMaxHp"
