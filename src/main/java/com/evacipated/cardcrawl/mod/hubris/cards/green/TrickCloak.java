7a33fdf93e0c51856c43fcbc4f865cc67b008a5c
Kiooeht <kiooeht@gmail.com>
2018-08-13 14:04:18 -0700

    Card: Trick Cloak

---
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/green/TrickCloak.java
@@ -0,0 +1,57 @@
+package com.evacipated.cardcrawl.mod.hubris.cards.green;
+
+import basemod.abstracts.CustomCard;
+import com.evacipated.cardcrawl.mod.stslib.actions.common.FetchAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.unique.RandomCardFromDiscardPileToHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class TrickCloak extends CustomCard
+{
+    public static final String ID = "hubris:TrickCloak";
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    private static final int COST = 1;
+    private static final int AMT = 1;
+
+    public TrickCloak()
+    {
+        super(ID, NAME, null,  COST, DESCRIPTION, CardType.SKILL, CardColor.GREEN, CardRarity.UNCOMMON, CardTarget.SELF);
+
+        magicNumber = baseMagicNumber = AMT;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        if (upgraded) {
+            AbstractDungeon.actionManager.addToBottom(new FetchAction(AbstractDungeon.player.discardPile, AMT));
+        } else {
+            AbstractDungeon.actionManager.addToBottom(new RandomCardFromDiscardPileToHandAction());
+        }
+        AbstractDungeon.actionManager.addToBottom(new DrawCardAction(AbstractDungeon.player, magicNumber));
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new TrickCloak();
+    }
+}
