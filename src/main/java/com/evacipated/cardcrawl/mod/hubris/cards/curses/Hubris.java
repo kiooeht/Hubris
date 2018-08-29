6345500a1887379608f7ab74d73cc59714fe9d41
Kiooeht <kiooeht@gmail.com>
2018-08-28 19:13:56 -0700

    Reduce Hubris cost to 0

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/curses/Hubris.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/curses/Hubris.java
@@ -21,7 +21,7 @@ public class Hubris extends CustomCard
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    private static final int COST = 1;
+    private static final int COST = 0;
 
     private static String[] sins = new String[]{Envy.ID, Gluttony.ID, Greed.ID, Lust.ID, Sloth.ID, Wrath.ID};
 
