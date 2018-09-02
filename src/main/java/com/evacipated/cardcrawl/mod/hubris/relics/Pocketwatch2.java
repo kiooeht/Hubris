f6d92455608dfea75c3d10cef6e9624d708572ef
Kiooeht <kiooeht@gmail.com>
2018-09-02 14:36:44 -0700

    Pocketwatch2: Make clickable instead of card

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/relics/Pocketwatch2.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/relics/Pocketwatch2.java
@@ -1,14 +1,15 @@
 package com.evacipated.cardcrawl.mod.hubris.relics;
 
-import com.evacipated.cardcrawl.mod.hubris.cards.colorless.TimeStop;
+import com.evacipated.cardcrawl.mod.hubris.powers.TimeStopPower;
+import com.evacipated.cardcrawl.mod.hubris.relics.abstracts.ClickableRelic;
 import com.evacipated.cardcrawl.mod.hubris.relics.abstracts.HubrisRelic;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
-import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
-public class Pocketwatch2 extends HubrisRelic
+public class Pocketwatch2 extends HubrisRelic implements ClickableRelic
 {
     public static final String ID = "hubris:Pocketwatch2";
     private static final int AMT = 6;
@@ -16,17 +17,12 @@ public class Pocketwatch2 extends HubrisRelic
     public Pocketwatch2()
     {
         super(ID, "pocketwatch.png", RelicTier.BOSS, LandingSound.HEAVY);
-
-        tips.clear();
-        tips.add(new PowerTip(name, description));
-        tips.add(new PowerTip(TimeStop.NAME, TimeStop.NAME + " is a #b" + TimeStop.COST + " cost Skill card. NL " + TimeStop.DESCRIPTION + TimeStop.EXTENDED_DESCRIPTION[0]));
-        initializeTips();
     }
 
     @Override
     public String getUpdatedDescription()
     {
-        return DESCRIPTIONS[0] + AMT + DESCRIPTIONS[1];
+        return DESCRIPTIONS[0];
     }
 
     @Override
@@ -36,12 +32,16 @@ public class Pocketwatch2 extends HubrisRelic
     }
 
     @Override
-    public void atBattleStartPreDraw()
+    public void onRightClick()
     {
-        if (counter > 0) {
-            flash();
-            AbstractDungeon.actionManager.addToBottom(new RelicAboveCreatureAction(AbstractDungeon.player, this));
-            AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(new TimeStop(), 1, true, false));
+        if (!isObtained || counter <= 0) {
+            return;
+        }
+
+        if (AbstractDungeon.getCurrRoom() != null && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) {
+            setCounter(counter - 1);
+            AbstractPlayer p = AbstractDungeon.player;
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new TimeStopPower(p, 1), 1));
         }
     }
 
