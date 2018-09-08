e1fabc2a7af782e321b0ddf43ee8f21230d69e92
Kiooeht <kiooeht@gmail.com>
2018-09-08 14:42:18 -0700

    Nice Rug: Make shops empty

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/relics/NiceRug.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/relics/NiceRug.java
@@ -1,11 +1,15 @@
 package com.evacipated.cardcrawl.mod.hubris.relics;
 
+import com.evacipated.cardcrawl.mod.hubris.fakes.FakeMerchant;
 import com.evacipated.cardcrawl.mod.hubris.relics.abstracts.HubrisRelic;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.rooms.ShopRoom;
 
 public class NiceRug extends HubrisRelic
 {
     public static final String ID = "hubris:NiceRug";
+    private ShopRoom shopRoom;
 
     public NiceRug()
     {
@@ -16,6 +20,25 @@ public class NiceRug extends HubrisRelic
     public String getUpdatedDescription()
     {
         return DESCRIPTIONS[0];
+    }
+
+    @Override
+    public void onEnterRoom(AbstractRoom room)
+    {
+        if (room instanceof ShopRoom) {
+            shopRoom = (ShopRoom) room;
+        }
+    }
+
+    @Override
+    public void update()
+    {
+        super.update();
+
+        if (shopRoom != null && shopRoom.merchant != null) {
+            shopRoom.merchant = new FakeMerchant(shopRoom.merchant);
+            shopRoom = null;
+        }
     }
 
     @Override
