e2b123448ccee3fed157024005e2d693de8f3465
Kiooeht <kiooeht@gmail.com>
2018-09-19 19:09:04 -0700

    Fix Lust crash from non-attack damage

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/powers/LustTargetPower.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/powers/LustTargetPower.java
@@ -46,7 +46,7 @@ public class LustTargetPower extends AbstractPower
     @Override
     public int onAttacked(DamageInfo info, int damageAmount)
     {
-        if (info.owner.isPlayer && info.type == DamageInfo.DamageType.NORMAL) {
+        if (info.owner != null && info.owner.isPlayer && info.type == DamageInfo.DamageType.NORMAL) {
             flash();
             AbstractDungeon.actionManager.addToBottom(new LoseHPAction(info.owner, owner, -amount));
         }
