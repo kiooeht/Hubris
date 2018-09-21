01699b3871fe79c7041bcbc6f51a8fe2a1927861
Kiooeht <kiooeht@gmail.com>
2018-09-21 16:34:39 -0700

    Fix Scarier Face + Reptomancer weird interaction

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/patches/ScarierMaskPatches.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/patches/ScarierMaskPatches.java
@@ -2,8 +2,13 @@ package com.evacipated.cardcrawl.mod.hubris.patches;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.actions.unique.SpawnDaggerAction;
 import com.megacrit.cardcrawl.actions.unique.SummonGremlinAction;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.monsters.beyond.Reptomancer;
+import javassist.CannotCompileException;
+import javassist.expr.ExprEditor;
+import javassist.expr.FieldAccess;
 
 public class ScarierMaskPatches
 {
@@ -11,7 +16,7 @@ public class ScarierMaskPatches
             clz=SummonGremlinAction.class,
             method="identifySlot"
     )
-    public static class GremlinLeader
+    public static class GremlinLeaderPatch
     {
         public static SpireReturn<Integer> Prefix(SummonGremlinAction __instance, AbstractMonster[] gremlins)
         {
@@ -21,6 +26,34 @@ public class ScarierMaskPatches
                 }
             }
             return SpireReturn.Continue();
+        }
+    }
+
+    @SpirePatch(
+            clz=Reptomancer.class,
+            method="canSpawn"
+    )
+    @SpirePatch(
+            clz=Reptomancer.class,
+            method="takeTurn"
+    )
+    @SpirePatch(
+            clz=SpawnDaggerAction.class,
+            method="update"
+    )
+    public static class ReptomancerPatch
+    {
+        public static ExprEditor Instrument()
+        {
+            return new ExprEditor() {
+                @Override
+                public void edit(FieldAccess f) throws CannotCompileException
+                {
+                    if (f.getFieldName().equals("isDying")) {
+                        f.replace("$_ = $proceed($$) || $0.isEscaping;");
+                    }
+                }
+            };
         }
     }
 }
