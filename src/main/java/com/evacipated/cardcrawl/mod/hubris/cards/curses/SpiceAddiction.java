a1655ced87a250bae4e4b273b211bf601b2baa1d
Kiooeht <kiooeht@gmail.com>
2018-09-20 17:29:50 -0700

    Add Spice relic description to Spice curse card

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/curses/SpiceAddiction.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/curses/SpiceAddiction.java
@@ -1,7 +1,9 @@
 package com.evacipated.cardcrawl.mod.hubris.cards.curses;
 
 import basemod.abstracts.CustomCard;
+import basemod.helpers.TooltipInfo;
 import com.evacipated.cardcrawl.mod.hubris.HubrisMod;
+import com.evacipated.cardcrawl.mod.hubris.relics.Spice;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -10,6 +12,9 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.BlueCandle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SpiceAddiction extends CustomCard
 {
@@ -20,9 +25,21 @@ public class SpiceAddiction extends CustomCard
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = -2;
 
+    private Spice tmpRelic;
+
     public SpiceAddiction()
     {
         super(ID, NAME, IMG, COST, DESCRIPTION, CardType.CURSE, CardColor.CURSE, CardRarity.SPECIAL, CardTarget.NONE);
+
+        tmpRelic = new Spice();
+    }
+
+    @Override
+    public List<TooltipInfo> getCustomTooltips()
+    {
+        List<TooltipInfo> ret = new ArrayList<>();
+        ret.add(new TooltipInfo(tmpRelic.name, tmpRelic.description));
+        return ret;
     }
 
     @Override
