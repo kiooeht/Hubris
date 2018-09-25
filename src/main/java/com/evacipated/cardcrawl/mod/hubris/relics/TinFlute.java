38111f11290a19e8e9b9d21621cdd0958ba8c1e4
Kiooeht <kiooeht@gmail.com>
2018-09-24 17:18:59 -0700

    Allow Tin Flute to save/load Duct Tape

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/relics/TinFlute.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/relics/TinFlute.java
@@ -2,20 +2,26 @@ package com.evacipated.cardcrawl.mod.hubris.relics;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
+import com.evacipated.cardcrawl.mod.hubris.cards.DuctTapeCard;
 import com.evacipated.cardcrawl.mod.hubris.relics.abstracts.HubrisRelic;
+import com.evacipated.cardcrawl.modthespire.lib.ConfigUtils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardSave;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.helpers.AsyncSaver;
 import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
-import com.megacrit.cardcrawl.saveAndContinue.SaveAndContinue;
 import com.megacrit.cardcrawl.saveAndContinue.SaveFileObfuscator;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class TinFlute extends HubrisRelic
 {
@@ -35,16 +41,25 @@ public class TinFlute extends HubrisRelic
 
     private static String getSavePath()
     {
-        return SaveAndContinue.SAVE_PATH + "hubris" + ".autosave" + (Settings.isBeta ? "BETA" : "");
+        return ConfigUtils.CONFIG_DIR + File.separator + "Hubris" + File.separator + "TinFlute" + ".autosave" + (Settings.isBeta ? "BETA" : "");
     }
 
     public static AbstractCard getSavedItem()
     {
-        if (Gdx.files.local(getSavePath()).exists()) {
+        if (Gdx.files.absolute(getSavePath()).exists()) {
             Gson gson = new Gson();
             String savestr = loadSaveString(getSavePath());
+            System.out.println(savestr);
             Save save = gson.fromJson(savestr, Save.class);
-            return CardLibrary.getCopy(save.tinFlute.id, save.tinFlute.upgrades, save.tinFlute.misc);
+            if (save.tinFlute.size() == 2) {
+                // Duct Tape
+                List<AbstractCard> cards = new ArrayList<>();
+                cards.add(CardLibrary.getCopy(save.tinFlute.get(0).id, save.tinFlute.get(0).upgrades, save.tinFlute.get(0).misc));
+                cards.add(CardLibrary.getCopy(save.tinFlute.get(1).id, save.tinFlute.get(1).upgrades, save.tinFlute.get(1).misc));
+                return new DuctTapeCard(cards);
+            } else {
+                return CardLibrary.getCopy(save.tinFlute.get(0).id, save.tinFlute.get(0).upgrades, save.tinFlute.get(0).misc);
+            }
         }
 
         return null;
@@ -52,12 +67,12 @@ public class TinFlute extends HubrisRelic
 
     public static void deleteSave()
     {
-        Gdx.files.local(getSavePath()).delete();
+        Gdx.files.absolute(getSavePath()).delete();
     }
 
     private static String loadSaveString(String filePath)
     {
-        FileHandle file = Gdx.files.local(filePath);
+        FileHandle file = Gdx.files.absolute(filePath);
         String data = file.readString();
         if (SaveFileObfuscator.isObfuscated(data)) {
             return SaveFileObfuscator.decode(data, "key");
@@ -80,14 +95,22 @@ public class TinFlute extends HubrisRelic
             AbstractCard card = AbstractDungeon.gridSelectScreen.selectedCards.get(0);
             chosen = true;
 
-            CardSave cardSave = new CardSave(card.cardID, card.timesUpgraded, card.misc);
             HashMap<Object, Object> params = new HashMap<>();
-
-            params.put("tinFlute", cardSave);
+            if (card instanceof DuctTapeCard) {
+                List<CardSave> cardSaves = ((DuctTapeCard) card).makeCardSaves();
+                params.put("tinFlute", cardSaves);
+            } else {
+                CardSave cardSave = new CardSave(card.cardID, card.timesUpgraded, card.misc);
+                params.put("tinFlute", cardSave);
+            }
 
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             String data = gson.toJson(params);
-            AsyncSaver.save(getSavePath(), data);
+            try {
+                Files.write(Paths.get(getSavePath()), data.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -99,9 +122,9 @@ public class TinFlute extends HubrisRelic
 
     private class Save
     {
-        public CardSave tinFlute;
+        public ArrayList<CardSave> tinFlute;
 
-        public Save(CardSave cardSave)
+        public Save(ArrayList<CardSave> cardSave)
         {
             tinFlute = cardSave;
         }
