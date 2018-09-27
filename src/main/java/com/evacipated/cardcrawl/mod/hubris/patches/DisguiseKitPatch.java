bcc2fd07443a3ba90feb0b48be5e5e2a77b07c9d
Kiooeht <kiooeht@gmail.com>
2018-09-27 12:40:11 -0700

    Fix Question Card + Cunning Disguise card rewards

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/patches/DisguiseKitPatch.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/patches/DisguiseKitPatch.java
@@ -3,15 +3,18 @@ package com.evacipated.cardcrawl.mod.hubris.patches;
 import com.evacipated.cardcrawl.mod.hubris.relics.DisguiseKit;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.rewards.RewardItem;
+import com.megacrit.cardcrawl.screens.CardRewardScreen;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class DisguiseKitPatch
 {
     @SpirePatch(
-            cls="com.megacrit.cardcrawl.rewards.RewardItem",
+            clz=RewardItem.class,
             method="claimReward"
     )
     public static class ClaimReward
@@ -25,7 +28,7 @@ public class DisguiseKitPatch
     }
 
     @SpirePatch(
-            cls="com.megacrit.cardcrawl.dungeons.AbstractDungeon",
+            clz=AbstractDungeon.class,
             method="getRewardCards"
     )
     public static class AddCardReward
@@ -44,6 +47,41 @@ public class DisguiseKitPatch
             }
 
             return __result;
+        }
+    }
+
+    private static Field PAD_X_f = null;
+
+    @SpirePatch(
+            clz=CardRewardScreen.class,
+            method="placeCards"
+    )
+    public static class PositionCards
+    {
+        public static void Prefix(CardRewardScreen __instance, float x, float y)
+        {
+            if (__instance.rewardGroup.size() == 5) {
+                float PAD_X = 40.0f * Settings.scale;
+                try {
+                    if (PAD_X_f == null) {
+                        PAD_X_f = CardRewardScreen.class.getDeclaredField("PAD_X");
+                        PAD_X_f.setAccessible(true);
+                    }
+                    PAD_X = PAD_X_f.getFloat(null);
+                } catch (IllegalAccessException | NoSuchFieldException ignored) {
+                }
+
+                __instance.rewardGroup.get(0).target_x = Settings.WIDTH / 2.0f - (AbstractCard.IMG_WIDTH + PAD_X) * 1.5f;
+                __instance.rewardGroup.get(1).target_x = Settings.WIDTH / 2.0f - (AbstractCard.IMG_WIDTH + PAD_X) * 0.75f;
+                __instance.rewardGroup.get(2).target_x = Settings.WIDTH / 2.0f;
+                __instance.rewardGroup.get(3).target_x = Settings.WIDTH / 2.0f + (AbstractCard.IMG_WIDTH + PAD_X) * 0.75f;
+                __instance.rewardGroup.get(4).target_x = Settings.WIDTH / 2.0f + (AbstractCard.IMG_WIDTH + PAD_X) * 1.5f;
+                __instance.rewardGroup.get(0).target_y = y;
+                __instance.rewardGroup.get(1).target_y = y;
+                __instance.rewardGroup.get(2).target_y = y;
+                __instance.rewardGroup.get(3).target_y = y;
+                __instance.rewardGroup.get(4).target_y = y;
+            }
         }
     }
 }
