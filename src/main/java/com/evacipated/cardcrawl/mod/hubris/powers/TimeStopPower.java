c7a2cf73f800fe49081fdf77fb3a0dc2bb122d9f
Kiooeht <kiooeht@gmail.com>
2018-10-03 16:17:40 -0700

    Stop Watch pauses enemy animations

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/powers/TimeStopPower.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/powers/TimeStopPower.java
@@ -1,5 +1,6 @@
 package com.evacipated.cardcrawl.mod.hubris.powers;
 
+import com.esotericsoftware.spine.AnimationState;
 import com.evacipated.cardcrawl.mod.hubris.actions.unique.StartNewTurnAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
@@ -7,9 +8,13 @@ import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.BlurPower;
 import com.megacrit.cardcrawl.powers.ConservePower;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TimeStopPower extends AbstractPower
 {
@@ -17,6 +22,8 @@ public class TimeStopPower extends AbstractPower
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    private Map<AbstractMonster, Float> timeScales;
 
     public TimeStopPower(AbstractCreature owner, int amount)
     {
@@ -33,6 +40,32 @@ public class TimeStopPower extends AbstractPower
     public void updateDescription()
     {
         description = DESCRIPTIONS[0];
+    }
+
+    @Override
+    public void onInitialApplication()
+    {
+        timeScales = new HashMap<>();
+        for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
+            AnimationState.TrackEntry e = m.state.getCurrent(0);
+            if (e != null) {
+                timeScales.put(m, e.getTimeScale());
+                e.setTimeScale(0);
+            }
+        }
+    }
+
+    @Override
+    public void onRemove()
+    {
+        for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
+            AnimationState.TrackEntry e = m.state.getCurrent(0);
+            if (e != null) {
+                if (timeScales.containsKey(m)) {
+                    e.setTimeScale(timeScales.get(m));
+                }
+            }
+        }
     }
 
     @Override
@@ -57,6 +90,22 @@ public class TimeStopPower extends AbstractPower
                 AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(owner, owner, new ConservePower(owner, 1), 1));
                 AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(owner, owner, new BlurPower(owner, 1), 1));
                 AbstractDungeon.actionManager.addToBottom(new StartNewTurnAction());
+            }
+        }
+    }
+
+    @Override
+    public void update(int slot)
+    {
+        super.update(slot);
+
+        for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
+            AnimationState.TrackEntry e = m.state.getCurrent(0);
+            if (e != null) {
+                if (!timeScales.containsKey(m)) {
+                    timeScales.put(m, e.getTimeScale());
+                }
+                e.setTimeScale(0);
             }
         }
     }
