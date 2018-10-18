2cbc12a2dc2ba0cbbd6f005596cb262968b36cf2
Kiooeht <kiooeht@gmail.com>
2018-10-17 20:37:10 -0700

    Fix Do While + Astral Hammer making an infinite loop

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/relics/AstralHammer.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/relics/AstralHammer.java
@@ -1,13 +1,14 @@
 package com.evacipated.cardcrawl.mod.hubris.relics;
 
 import com.evacipated.cardcrawl.mod.hubris.relics.abstracts.HubrisRelic;
+import com.evacipated.cardcrawl.mod.stslib.relics.OnAfterUseCardRelic;
 import com.megacrit.cardcrawl.actions.common.ReduceCostAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
-public class AstralHammer extends HubrisRelic
+public class AstralHammer extends HubrisRelic implements OnAfterUseCardRelic
 {
     public static final String ID = "hubris:AstralHammer";
     private static final int COST_DECREASE = -1;
@@ -24,7 +25,7 @@ public class AstralHammer extends HubrisRelic
     }
 
     @Override
-    public void onUseCard(AbstractCard card, UseCardAction action)
+    public void onAfterUseCard(AbstractCard card, UseCardAction action)
     {
         if (card.type == AbstractCard.CardType.ATTACK) {
             flash();
