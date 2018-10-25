b6dd508778be16619d6bd624136b231928036f3c
Kiooeht <kiooeht@gmail.com>
2018-10-24 22:36:32 -0700

    Blood Shop: Make map and deck views work

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/patches/BloodShopPatch.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/patches/BloodShopPatch.java
@@ -9,10 +9,15 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.EventHelper;
+import com.megacrit.cardcrawl.helpers.controller.CInputActionSet;
+import com.megacrit.cardcrawl.helpers.input.InputActionSet;
+import com.megacrit.cardcrawl.helpers.input.InputHelper;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.megacrit.cardcrawl.rooms.ShopRoom;
+import com.megacrit.cardcrawl.screens.CardRewardScreen;
 import com.megacrit.cardcrawl.shop.Merchant;
 import com.megacrit.cardcrawl.shop.ShopScreen;
+import com.megacrit.cardcrawl.ui.panels.TopPanel;
 import javassist.CannotCompileException;
 import javassist.CtBehavior;
 import javassist.expr.ExprEditor;
@@ -24,7 +29,7 @@ public class BloodShopPatch
 {
     //*
     @SpirePatch(
-            clz= Merchant.class,
+            clz=Merchant.class,
             method=SpirePatch.CONSTRUCTOR,
             paramtypez={
                     float.class,
@@ -59,7 +64,7 @@ public class BloodShopPatch
         }
     }
     @SpirePatch(
-            clz= Merchant.class,
+            clz=Merchant.class,
             method="update"
     )
     public static class ScreenOpen
@@ -198,6 +203,89 @@ public class BloodShopPatch
                     AbstractDungeon.overlayMenu.showCombatPanels();
                 }
                 AbstractDungeon.overlayMenu.cancelButton.hide();
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz=TopPanel.class,
+            method="updateDeckViewButtonLogic"
+    )
+    public static class DeckViewButton
+    {
+        @SpireInsertPatch(
+                locator=Locator.class,
+                localvars={"deckButtonDisabled"}
+        )
+        public static SpireReturn<Void> Insert(TopPanel __instance, @ByRef boolean[] deckButtonDisabled)
+        {
+            if (AbstractDungeon.screen == BloodShopScreen.Enum.HUBRIS_BLOOD_SHOP) {
+                deckButtonDisabled[0] = false;
+                __instance.deckHb.update();
+
+                if (((__instance.deckHb.hovered && InputHelper.justClickedLeft)
+                            || InputActionSet.masterDeck.isJustPressed()
+                            || CInputActionSet.pageLeftViewDeck.isJustPressed())
+                        && !CardCrawlGame.isPopupOpen) {
+                    AbstractDungeon.overlayMenu.cancelButton.hide();
+                    AbstractDungeon.previousScreen = BloodShopScreen.Enum.HUBRIS_BLOOD_SHOP;
+                    AbstractDungeon.deckViewScreen.open();
+
+                    InputHelper.justClickedLeft = false;
+                }
+                return SpireReturn.Return(null);
+            }
+            return SpireReturn.Continue();
+        }
+
+        private static class Locator extends SpireInsertLocator
+        {
+            @Override
+            public int[] Locate(CtBehavior ctMethodToPatch) throws Exception
+            {
+                Matcher finalMatcher = new Matcher.FieldAccessMatcher(InputHelper.class, "justClickedLeft");
+                return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz=TopPanel.class,
+            method="updateMapButtonLogic"
+    )
+    public static class MapButton
+    {
+        @SpireInsertPatch(
+                locator=Locator.class,
+                localvars={"mapButtonDisabled"}
+        )
+        public static SpireReturn<Void> Insert(TopPanel __instance, @ByRef boolean[] deckButtonDisabled)
+        {
+            if (AbstractDungeon.screen == BloodShopScreen.Enum.HUBRIS_BLOOD_SHOP) {
+                deckButtonDisabled[0] = false;
+                __instance.mapHb.update();
+
+                if ((__instance.mapHb.hovered && InputHelper.justClickedLeft)
+                        || InputActionSet.map.isJustPressed()
+                        || CInputActionSet.map.isJustPressed()) {
+                    AbstractDungeon.overlayMenu.cancelButton.hide();
+                    AbstractDungeon.previousScreen = BloodShopScreen.Enum.HUBRIS_BLOOD_SHOP;
+                    AbstractDungeon.dungeonMapScreen.open(false);
+
+                    InputHelper.justClickedLeft = false;
+                }
+                return SpireReturn.Return(null);
+            }
+            return SpireReturn.Continue();
+        }
+
+        private static class Locator extends SpireInsertLocator
+        {
+            @Override
+            public int[] Locate(CtBehavior ctMethodToPatch) throws Exception
+            {
+                Matcher finalMatcher = new Matcher.FieldAccessMatcher(InputHelper.class, "justClickedLeft");
+                return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
             }
         }
     }
