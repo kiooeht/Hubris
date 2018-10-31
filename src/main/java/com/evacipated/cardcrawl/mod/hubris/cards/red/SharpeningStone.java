5681aab7c592c436f033d80cb11c014bf745d85c
Kiooeht <kiooeht@gmail.com>
2018-10-31 15:51:09 -0700

    Tweak Sharpening Stone

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/red/SharpeningStone.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/cards/red/SharpeningStone.java
@@ -3,7 +3,8 @@ package com.evacipated.cardcrawl.mod.hubris.cards.red;
 import basemod.abstracts.CustomCard;
 import com.evacipated.cardcrawl.mod.hubris.HubrisMod;
 import com.evacipated.cardcrawl.mod.hubris.actions.unique.SharpeningStoneAction;
-import com.evacipated.cardcrawl.mod.stslib.fields.cards.AbstractCard.ExhaustiveField;
+import com.evacipated.cardcrawl.mod.hubris.actions.unique.SharpeningStoneRandomAction;
+import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.StartupCard;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -11,7 +12,7 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-public class SharpeningStone extends CustomCard
+public class SharpeningStone extends CustomCard implements StartupCard
 {
     public static final String ID = "hubris:SharpeningStone";
     public static final String IMG = HubrisMod.BETA_SKILL;
@@ -19,9 +20,8 @@ public class SharpeningStone extends CustomCard
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
-    private static final int COST = 1;
-    private static final int AMT = 1;
-    private static final int UPGRADE_AMT = 1;
+    private static final int COST = 0;
+    private static final int AMT = 2;
 
     public SharpeningStone()
     {
@@ -29,6 +29,16 @@ public class SharpeningStone extends CustomCard
 
         exhaust = true;
         magicNumber = baseMagicNumber = AMT;
+    }
+
+    @Override
+    public boolean atBattleStartPreDraw()
+    {
+        if (upgraded) {
+            AbstractDungeon.actionManager.addToBottom(new SharpeningStoneRandomAction(AbstractDungeon.player, 1));
+            return true;
+        }
+        return false;
     }
 
     @Override
@@ -42,8 +52,7 @@ public class SharpeningStone extends CustomCard
     {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_AMT);
-            rawDescription = UPGRADE_DESCRIPTION;
+            rawDescription = DESCRIPTION + UPGRADE_DESCRIPTION;
             initializeDescription();
         }
     }
