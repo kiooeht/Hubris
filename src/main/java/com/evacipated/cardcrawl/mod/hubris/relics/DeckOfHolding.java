6c029af498b9cc20d92bed9fec96f09d3a0b27f9
Kiooeht <kiooeht@gmail.com>
2018-11-11 14:12:22 -0800

    Give Deck of Holding a synergy with Runic Pyramid

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/relics/DeckOfHolding.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/relics/DeckOfHolding.java
@@ -4,8 +4,13 @@ import com.evacipated.cardcrawl.mod.hubris.powers.RetainHandPower;
 import com.evacipated.cardcrawl.mod.hubris.relics.abstracts.HubrisRelic;
 import com.evacipated.cardcrawl.mod.stslib.relics.ClickableRelic;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.helpers.RelicLibrary;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.relics.RunicPyramid;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 public class DeckOfHolding extends HubrisRelic implements ClickableRelic
@@ -16,12 +21,18 @@ public class DeckOfHolding extends HubrisRelic implements ClickableRelic
     public DeckOfHolding()
     {
         super(ID, "deckOfHolding.png", RelicTier.UNCOMMON, LandingSound.FLAT);
+
+        tips.clear();
+        tips.add(new PowerTip(name, description));
+        tips.add(new PowerTip("Synergy",
+                DESCRIPTIONS[1] + FontHelper.colorString(new RunicPyramid().name, "y") + DESCRIPTIONS[2]));
+        initializeTips();
     }
 
     @Override
     public String getUpdatedDescription()
     {
-        return CLICKABLE_DESCRIPTIONS()[0] + DESCRIPTIONS[0];
+        return CLICKABLE_DESCRIPTIONS()[0] + DESCRIPTIONS[0] + new RunicPyramid().name;
     }
 
     @Override
@@ -35,8 +46,15 @@ public class DeckOfHolding extends HubrisRelic implements ClickableRelic
             usedThisCombat = true;
             flash();
             stopPulse();
-            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player,
-                    new RetainHandPower(AbstractDungeon.player)));
+            if (AbstractDungeon.player.hasRelic(RunicPyramid.ID)) {
+                int count = AbstractDungeon.player.hand.size();
+                if (count != 0) {
+                    AbstractDungeon.actionManager.addToBottom(new DiscardAction(AbstractDungeon.player, AbstractDungeon.player, count, true));
+                }
+            } else {
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player,
+                        new RetainHandPower(AbstractDungeon.player)));
+            }
         }
     }
 
