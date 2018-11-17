b3088bf1a9e6a7ca187631b59baf2b0da04a092c
Kiooeht <kiooeht@gmail.com>
2018-11-17 14:48:49 -0800

    Stop fake death power from being removed

--- src/main/java/com/evacipated/cardcrawl/mod/hubris/powers/FakeDeathPower.java
+++ src/main/java/com/evacipated/cardcrawl/mod/hubris/powers/FakeDeathPower.java
@@ -2,6 +2,7 @@ package com.evacipated.cardcrawl.mod.hubris.powers;
 
 import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.InvisiblePower;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
@@ -37,5 +38,14 @@ public class FakeDeathPower extends AbstractPower implements InvisiblePower
         owner.halfDead = true;
         ((AbstractMonster) owner).intent = AbstractMonster.Intent.NONE;
         owner.tint.fadeOut();
+    }
+
+    @Override
+    public void onRemove()
+    {
+        // Add a copy, only one will be removed
+        owner.powers.add(0, this);
+        // Cancel the removal text effect
+        AbstractDungeon.effectList.remove(AbstractDungeon.effectList.size() - 1);
     }
 }
